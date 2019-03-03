7fd2ae482a1cb963f6ef89640b82051ac2adab36
Simon Pham <simon@hasbrain.com>
2019-03-03 17:39:10 +0700

    Add HomeFragment

---
+++ app/src/main/java/com/github/simonpham/sunshine/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.github.simonpham.sunshine;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Simon Pham on 3/3/19.
+ * Email: simonpham.dn@gmail.com
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
