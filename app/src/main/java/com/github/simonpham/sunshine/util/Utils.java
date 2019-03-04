e449a3af33d90c124f56832ac0b02b07051e21ba
Simon Pham <simon@hasbrain.com>
2019-03-04 18:32:55 +0700

    Add getDayName util

--- app/src/main/java/com/github/simonpham/sunshine/util/Utils.java
+++ app/src/main/java/com/github/simonpham/sunshine/util/Utils.java
@@ -1,6 +1,13 @@
 package com.github.simonpham.sunshine.util;
 
+import android.content.Context;
+
 import com.github.simonpham.sunshine.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Simon Pham on 3/3/19.
@@ -8,9 +15,49 @@ import com.github.simonpham.sunshine.R;
  */
 public class Utils {
 
+    public static int julianDay(long dateInMillis) {
+        Date date = new Date(dateInMillis);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int a = (14 - month) / 12;
+        int y = year + 4800 - a;
+        int m = month + 12 * a - 3;
+        return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    }
+
+    /**
+     * Given a day, returns just the name to use for that day.
+     * E.g "today", "tomorrow", "wednesday".
+     *
+     * @param context      Context to use for resource localization
+     * @param dateInMillis The date in milliseconds
+     * @return
+     */
+    public static String getDayName(Context context, long dateInMillis) {
+        // If the date is today, return the localized version of "Today" instead of the actual
+        // day name.
+        long currentDate = System.currentTimeMillis();
+        int julianDay = julianDay(dateInMillis * 1000);
+        int currentJulianDay = julianDay(currentDate);
+        if (julianDay == currentJulianDay) {
+            return context.getString(R.string.today);
+        } else if (julianDay == currentJulianDay + 1) {
+            return context.getString(R.string.tomorrow);
+        } else {
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
+            return dayFormat.format(dateInMillis * 1000);
+        }
+    }
+
     /**
      * Helper method to provide the icon resource id according to the weather condition id returned
      * by the OpenWeatherMap call.
+     *
      * @param weatherId from OpenWeatherMap API response
      * @return resource id for the corresponding icon. -1 if no relation is found.
      */
@@ -46,6 +93,7 @@ public class Utils {
     /**
      * Helper method to provide the art resource id according to the weather condition id returned
      * by the OpenWeatherMap call.
+     *
      * @param weatherId from OpenWeatherMap API response
      * @return resource id for the corresponding icon. -1 if no relation is found.
      */
